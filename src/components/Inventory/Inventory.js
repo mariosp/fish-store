326fddc867840bee469f17f1bf65a45efa368479
Marios Patsis <mariospatsis@gmail.com>
2020-04-25 14:11:34 +0300

    form component

--- src/components/Inventory/Inventory.js
+++ src/components/Inventory/Inventory.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
 import './Inventory.css';
+import InventoryForm from "./InventoryForm/InventoryForm";
 
 const Inventory = () => {
+    const [addButton, SetAddButton] = useState(false);
+
+    const addNew = (product) => {
+      const validForm = Object.values(product).every((value,index) => {
+          if (index ===1 && isNaN(value)) {
+              return false;
+          }
+          return value.length > 0;
+
+      });
+      SetAddButton(validForm);
+    };
+
     return (
         <div className="inventory-container">
             <div className="inventory-container-list">
@@ -13,7 +27,8 @@ const Inventory = () => {
                 <div className="inventory-header">
                     <div className="inventory-header-title">ADD A NEW FISH</div>
                 </div>
-
+                <InventoryForm onchange={addNew} />
+                <button disabled={!addButton} className="add-btn">ADD FISH</button>
             </div>
         </div>
     );
--- src/components/Inventory/InventoryForm/InventoryForm.js
+++ src/components/Inventory/InventoryForm/InventoryForm.js
@@ -1,10 +1,57 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import './InventoryForm.css';
+import {productStatus} from "../../../Shared/product-status";
 
-const InventoryForm = () => {
+const ProductOptions = ()=> (
+    productStatus.map(status => (<option key={status} value={status}>{status}</option>))
+);
+
+
+const InventoryForm = ({onchange}) => {
+    const [product, setProduct] = useState({
+        Title:'',
+        Price: '',
+        Status: 'available',
+        Description: '',
+        ImageUrl: ''
+    });
+
+    useEffect(()=>{
+        onchange(product)
+    },[product]);
+
+    const handleChange = (event) => {
+        event.persist();
+        setProduct(value => ({...value, [event.target.name]: event.target.value}));
+    };
+
+    // console.log(product)
+
     return (
-        <div>
-            Form
+        <div className="form-container">
+            <div className="row">
+                <div  className="col col-left">
+                    <input name="Title" placeholder="Name" type="text" value={product.Title} onChange={handleChange} />
+                </div>
+                <div  className="col col-center">
+                    <input name="Price" placeholder="Price" type="number" min="0" value={product.Price} onChange={handleChange}/>
+                </div>
+                <div  className="col col-right">
+                    <select name="Status" placeholder="Availability" value={product.Status} onChange={handleChange}>
+                        <ProductOptions />
+                    </select>
+                </div>
+            </div>
+            <div className="row">
+                <div  className="col col-center">
+                    <textarea name="Description" placeholder="Descr." value={product.Description} onChange={handleChange} />
+                </div>
+            </div>
+            <div className="row">
+                <div  className="col col-center">
+                    <input name="ImageUrl" placeholder="Image" type="text" value={product.ImageUrl} onChange={handleChange}/>
+                </div>
+            </div>
         </div>
     )
 };
